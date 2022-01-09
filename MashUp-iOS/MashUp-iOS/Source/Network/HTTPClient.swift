c940658d150d548b4b86eeccc1109f8b8385a282
dongyoung.lee <dongyoung.lee@naverz-corp.com>
2022-01-10 01:51:43 +0900

    :sparkles: Network 레이어 구현

--- MashUp-iOS/MashUp-iOS/Source/Network/HTTPClient.swift
+++ MashUp-iOS/MashUp-iOS/Source/Network/HTTPClient.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+import Moya
+import RxMoya
+
+protocol Network {
+    func request<API: MashUpAPI>(_ api: API) -> Observable<API.Response>
+}
+
+final class HTTPClient: Network {
+    
+    func request<API>(_ api: API) -> Observable<API.Response> where API : MashUpAPI {
+        let erasedAPI = MultiTarget(api)
+        
+        return self.provider.rx.request(erasedAPI)
+            .asObservable()
+            .compactMap { [weak self] in
+                try self?.decoder.decode(ResponseModel<API.Response>.self, from: $0.data).data
+            }
+    }
+    
+    private let provider = MoyaProvider<MultiTarget>()
+    private let decoder = JSONDecoder()
+}
