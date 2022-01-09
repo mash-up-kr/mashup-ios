02798c5922e95fa93841537573142790e4d785f4
dongyoung.lee <dongyoung.lee@naverz-corp.com>
2022-01-10 00:56:41 +0900

    :tada: 프로젝트 생성

---
+++ MashUp-iOS/MashUp-iOS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MashUp-iOS
+//
+//  Created by Booung on 2022/01/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
