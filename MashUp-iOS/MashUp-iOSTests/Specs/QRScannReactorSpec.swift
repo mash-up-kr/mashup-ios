5620582ef4b31202f1bd7c4bb07d3bdfbea5cae1
dongyoung.lee <dongyoung.lee@naverz-corp.com>
2022-02-21 01:56:13 +0900

    QRScanReactor 테스트 코드 작성

--- MashUp-iOS/MashUp-iOSTests/Specs/QRScannReactorSpec.swift
+++ MashUp-iOS/MashUp-iOSTests/Specs/QRScannReactorSpec.swift
@@ -7,31 +7,72 @@
 //
 
 
+import AVFoundation
+import Mockingbird
+import Nimble
 import Quick
 import RxBlocking
 @testable import MashUp_iOS
-import Mockingbird
 
 final class QRScannReactorSpec: QuickSpec {
   
   override func spec() {
     var sut: QRScanReactor!
+    var captureSessionDummy: AVCaptureSession!
     var qrReaderMock: QRReaderServiceMock!
     var attendanceServiceMock: AttendanceServiceMock!
     
     beforeEach {
+      captureSessionDummy = AVCaptureSession()
       qrReaderMock = mock(QRReaderService.self)
       attendanceServiceMock = mock(AttendanceService.self)
-      sut = QRScanReactor(qrReader: qrReaderMock, attendanceService: attendanceServiceMock)
     }
-    
     describe("QRScanReactor") {
+      beforeEach {
+        given(qrReaderMock.scanCode()).willReturn(.empty())
+        given(qrReaderMock.captureSession).willReturn(captureSessionDummy)
+        sut = QRScanReactor(qrReader: qrReaderMock, attendanceService: attendanceServiceMock)
+      }
       context("when did set up") {
+        beforeEach {
+          sut.action.onNext(.didSetup)
+        }
         it("qr reader is ready to scan code") {
-          
+          verify(qrReaderMock.scanCode()).wasCalled()
+        }
+        context("when capture code from session") {
+          let stubbedCode = "stubbed.code"
+          let correctCode: String = "correct.code"
+          let wrongCode: String = "wrong.code"
+          beforeEach {
+            given(qrReaderMock.scanCode()).willReturn(.just(stubbedCode))
+            given(attendanceServiceMock.attend(withCode: any())).willReturn(.just(false))
+            given(attendanceServiceMock.attend(withCode: correctCode)).willReturn(.just(true))
+          }
+          it("request attendence with captured code") {
+            sut.action.onNext(.didSetup)
+            verify(attendanceServiceMock.attend(withCode: stubbedCode)).wasCalled()
+          }
+          context("requesting attendance with correct code") {
+            beforeEach {
+              given(qrReaderMock.scanCode()).willReturn(.just(correctCode))
+            }
+            it("attendance did success") {
+              sut.action.onNext(.didSetup)
+              expect { sut.currentState.alertMessage }.to(equal("✅ 출석을 완료하셨습니다."))
+            }
+          }
+          context("requesting attendance with wrong code") {
+            beforeEach {
+              given(qrReaderMock.scanCode()).willReturn(.just(wrongCode))
+            }
+            it("attendance did failure") {
+              sut.action.onNext(.didSetup)
+              expect { sut.currentState.alertMessage }.to(equal("❌ 올바른 코드가 아닙니다."))
+            }
+          }
         }
       }
     }
   }
-  
 }
