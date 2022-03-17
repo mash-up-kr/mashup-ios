30472f70cdd142ef21cb17f61bb37ca392db09a0
namsoo5 <are34@naver.com>
2022-03-17 16:49:14 +0900

    :recycle: AttendanceMember 변경
    - 참석상태 프로퍼티 추가

--- MashUp-iOS/MashUp-iOS/Source/Domain/Attendance/AttendanceMember.swift
+++ MashUp-iOS/MashUp-iOS/Source/Domain/Attendance/AttendanceMember.swift
@@ -11,7 +11,9 @@ import Foundation
 struct AttendanceMember {
     let name: String
     let platform: PlatformTeam
+    let firstSeminarAttendance: AttendanceStyle
     let firstSeminarAttendanceTime: Date?
+    let secondSeminarAttendance: AttendanceStyle
     let secondSeminarAttendanceTime: Date?
 }
 
@@ -19,15 +21,21 @@ extension AttendanceMember {
     static let dummy: [AttendanceMember] = [
         AttendanceMember(name: "김남수",
                          platform: .iOS,
+                         firstSeminarAttendance: .attend,
                          firstSeminarAttendanceTime: Date(),
+                         secondSeminarAttendance: .attend,
                          secondSeminarAttendanceTime: Date()),
         AttendanceMember(name: "김남수1",
                          platform: .iOS,
+                         firstSeminarAttendance: .lateness,
                          firstSeminarAttendanceTime: Date().addingTimeInterval(10000),
+                         secondSeminarAttendance: .attend,
                          secondSeminarAttendanceTime: Date().addingTimeInterval(16000)),
         AttendanceMember(name: "김남수2",
                          platform: .android,
+                         firstSeminarAttendance: .attend,
                          firstSeminarAttendanceTime: Date(),
+                         secondSeminarAttendance: .absence,
                          secondSeminarAttendanceTime: Date()),
     ]
 }
