34064c3ad90174f6475b9e4acd8d34025be2864f
namsoo5 <are34@naver.com>
2022-05-26 16:02:47 +0900

    :sparkles: 팀이라는 네이밍보다 플랫폼에 관한 구현체들을 포함하도록 폴더명 변경

---
+++ MashUp-iOS/MashUp-iOS/Source/Domain/Platform/PlatformAttendance.swift
@@ -0,0 +1,16 @@
+//
+//  PlatformAttendance.swift
+//  MashUp-iOS
+//
+//  Created by 남수김 on 2022/05/26.
+//  Copyright © 2022 Mash Up Corp. All rights reserved.
+//
+
+import Foundation
+
+struct PlatformAttendance {
+    let platform: PlatformTeam
+    let attendNumber: Int
+    let latenessNumber: Int
+    let absenceNumber: Int
+}
