dee3d1e05225093f315abe5a2d1164c9d2f4ca60
namsoo5 <are34@naver.com>
2022-05-26 18:33:10 +0900

    :sparkles: 플랫폼 출석현황 리액터 구현

---
+++ MashUp-iOS/MashUp-iOS/Source/Screen/PlatformAttendanceStatus/PlatformAttendanceStatusReactor.swift
@@ -0,0 +1,62 @@
+//
+//  PlatformAttendanceStatusReactor.swift
+//  MashUp-iOS
+//
+//  Created by 남수김 on 2022/05/26.
+//  Copyright © 2022 Mash Up Corp. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+
+final class PlatformAttendanceStatusReactor: Reactor {
+    enum Action {
+        case didSetup
+        case didTapPlatform(PlatformTeam)
+    }
+    
+    enum Mutation {
+        case updatePlatformsAttendance([PlatformAttendance])
+        case updateSelectedPlatform(PlatformTeam)
+    }
+    
+    struct State {
+        var platformsAttendance: [PlatformAttendance] = []
+        var selectedPlatform: PlatformTeam?
+    }
+    
+    let initialState: State
+    let platformService: PlatformService
+    
+    init(platformService: PlatformService) {
+        initialState = State()
+        self.platformService = platformService
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .didSetup:
+            return requestPlatformsAttendance()
+        case .didTapPlatform(let platform):
+            return .just(.updateSelectedPlatform(platform))
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .updatePlatformsAttendance(let platformsAttendance):
+            state.platformsAttendance = platformsAttendance
+        case .updateSelectedPlatform(let platform):
+            state.selectedPlatform = platform
+        }
+        return state
+    }
+    
+    private func requestPlatformsAttendance() -> Observable<Mutation> {
+        return platformService.attendanceStatus()
+            .flatMap { status -> Observable<Mutation> in
+                    return .just(.updatePlatformsAttendance(status))
+            }
+    }
+}
