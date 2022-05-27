79807b5557a3255935380dcaa233c6e465864c48
namsoo5 <are34@naver.com>
2022-05-27 17:39:34 +0900

    :sparkles: 폰트추가

---
+++ MashUp-iOS/MashUp-iOS/Source/Common/UIKit+/UIFont+.swift
@@ -0,0 +1,24 @@
+//
+//  UIFont+.swift
+//  MashUp-iOS
+//
+//  Created by 남수김 on 2022/05/27.
+//  Copyright © 2022 Mash Up Corp. All rights reserved.
+//
+
+import UIKit
+
+enum PretendardFontWeight {
+    case regular, medium, semiBold, bold
+}
+
+extension UIFont {
+    func pretendardBold(weight: PretendardFontWeight, size: CGFloat) -> UIFont {
+        switch weight {
+        case .regular: return UIFont(name: "Pretendard-Regular", size: size)!
+        case .medium: return UIFont(name: "Pretendard-Medium", size: size)!
+        case .semiBold: return UIFont(name: "Pretendard-SemiBold", size: size)!
+        case .bold: return UIFont(name: "Pretendard-Bold", size: size)!
+        }
+    }
+}
