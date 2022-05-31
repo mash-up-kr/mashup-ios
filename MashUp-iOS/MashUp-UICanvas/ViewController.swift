d067f0e40eac90fb4b2ae549d650b5209e57dd7d
dongyoung.lee <dongyoung.lee@naverz-corp.com>
2022-06-01 00:59:10 +0900

    ✨ MashUp UI테스트용 Canvas앱 타겟 추가

---
+++ MashUp-iOS/MashUp-UICanvas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MashUp-UICanvas
+//
+//  Created by Booung on 2022/06/01.
+//  Copyright © 2022 Mash Up Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
